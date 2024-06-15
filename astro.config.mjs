ffad2cd147fa11fc702e972c51735b856abdcc6d
Mukesh Murugan <iammukeshm@gmail.com>
2024-06-15 18:44:16 +0530

    update style

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,36 @@
-import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [starlight({
+    title: 'fullstackhero',
+    social: {
+      github: 'https://github.com/withastro/starlight'
+    },
+    customCss: [
+      './src/styles/tailwind.css',
+      './src/styles/base.css'
+    ],
+    sidebar: [{
+      label: 'Getting Started',
+      items: [
+        {
+          label: 'Example Guide',
+          link: '/guides/example/'
+        }]
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }]
+  }),
+  tailwind(
+    {
+      applyBaseStyles: false,
+    }
+  )]
 });
